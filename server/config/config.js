8f34e0bf741febe985577edb0c2443894a0e00fe
Andres <andres.fe21@hotmail.com>
2019-09-20 15:56:05 -0500

    promesa conecxion prueba con contrasena normal

--- server/config/config.js
+++ server/config/config.js
@@ -4,12 +4,13 @@ process.env.PORT = process.env.PORT || 3000;
 
 //entorno
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
 let urlDB;
 
 // if (process.env.NODE_ENV === 'dev') {
 //     urlDB = 'mongodb: //localhost:27017/cafe';
 // } else {
-urlDB = 'mongodb + srv: //strider:dPfXAvq0cVIm497r@cluster0-r8bkc.mongodb.net/cafe';
+urlDB = 'mongodb + srv: //strider:123456@cluster0-r8bkc.mongodb.net/cafe';
 // }
 
 process.env.URL_DB = urlDB;
