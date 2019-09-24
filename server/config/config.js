c74a17e4f6e39969faa235d6dbce6db7e676ee3d
Andres <andres.fe21@hotmail.com>
2019-09-24 11:11:09 -0500

    prueba con retrywrites

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://strider:dPfXAvq0cVIm497r@cluster0-r8bkc.mongodb.net/cafe';
+    urlDB = 'mongodb+srv://strider:dPfXAvq0cVIm497r@cluster0-r8bkc.mongodb.net/cafe?retryWrites=true&w=majority';
 }
 
 process.env.URL_DB = urlDB;
